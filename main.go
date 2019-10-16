b0f70d5b847d1e78521182b6fe93275d427cc2cc
Patrik <patrik.sirol@actual-it.si>
2019-10-16 15:55:19 +0200

    Formatted strings properly

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var (
 )
 
 func main() {
+
 	//Parse flags and set defaults in case there's no options provided when running the app
 	flag.IntVar(&ActiveFrom, "ActiveFrom", 7, "Hour from when notifications become active")
 	flag.IntVar(&ActiveTo, "ActiveTo", 17, "Hour when notifications become inactive")
@@ -29,8 +30,9 @@ func main() {
 		os.Exit(1)
 	}
 
+	//Get current time and print out as a logger
 	hour, minute, second := time.Now().Clock()
-	fmt.Println("Application started at time:", hour, ":", minute, ":", second, ". Next notification at:", hour+Interval, ":", minute, ":", second)
+	fmt.Println(fmt.Sprintf("Application started at time: %d:%d:%d. Next notification at: %d:%d:%d", hour, minute, second, hour+Interval, minute, second))
 
 	for {
 		//Delay by the interval
@@ -49,6 +51,6 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("New notification at:", hour, ":", minute, ":", second, "! Next notification at:", hour+Interval, ":", minute, ":", second)
+		fmt.Println(fmt.Sprintf("New notification at: %d:%d:%d ! Next notification at: %d:%d:%d", hour, minute, second, hour+Interval, minute, second))
 	}
 }
